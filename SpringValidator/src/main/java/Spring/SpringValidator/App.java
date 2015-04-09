2c3c39ddd8afe00eb3844a96f10c7d63a91ea023
Vamsi <vamsyraju@gmail.com>
2015-04-09 09:47:23 -0500

    Commit from Command Line

--- SpringValidator/src/main/java/Spring/SpringValidator/App.java
+++ SpringValidator/src/main/java/Spring/SpringValidator/App.java
@@ -8,6 +8,6 @@ public class App
 {
     public static void main( String[] args )
     {
-        System.out.println( "Hello World First prog change!" );
+        System.out.println( "Hello World First prasog change!" );
     }
 }
