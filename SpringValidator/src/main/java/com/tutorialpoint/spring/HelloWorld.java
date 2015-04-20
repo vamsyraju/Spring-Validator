6986cb8ec49ec9d9af1ac053df87f83f4284bcf9
Vamsi <vamsyraju@gmail.com>
2015-04-20 13:30:47 -0500

    Adding Spring Context file

---
+++ SpringValidator/src/main/java/com/tutorialpoint/spring/HelloWorld.java
@@ -0,0 +1,13 @@
+package com.tutorialpoint.spring;
+
+public class HelloWorld {
+	private String message;
+
+	public void getMessage() {
+		System.out.println("Your Message : " +message);
+	}
+
+	public void setMessage(String message) {
+		this.message = message;
+	}
+}
